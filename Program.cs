7422d15cec3e81212138126f9b5b972f75b5ea38
Emma Cluff <dragonbotguardian@gmail.com>
2016-10-28 11:58:22 -0600

    added level and obsticals

--- Program.cs
+++ Program.cs
@@ -2,12 +2,10 @@
 
 public class Program
 {
+    public static bool canPlay = true;
     public static Game myGame = new Game();
     public static void Main(string[] args)
     {
-        Console.WriteLine("Please type in your name:");        
-        myGame.name = Console.ReadLine();
-        Console.WriteLine("Your player name is " + myGame.name);
         myGame.Start();
     }
 }
--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -5,10 +5,40 @@ public class Game{
         Health.power = 100;
         Health.message = "You are getting stronger.";
         Ammo.message = "You have more ammo.";
+        Cave.StartMessage = "You have entered a cave.";
     }
 
+    //Runs at the start of the game
     public void Start (){
-        Health.RunPowerUp();
+        Console.WriteLine("Please type in your name:");        
+        name = Console.ReadLine();
+        Console.WriteLine("Your player name is " + name);
+        while(Program.canPlay){
+            Walk();
+        }
+        Console.WriteLine("You died.");
+    }
+
+    private void Walk(){
+        Random randomNum = new Random();
+        Cave.Enter();
+        Cave.Encounter(randomNum.Next(0, Cave.objects.Length));
+    }
+
+    //Game Levels
+    private LevelBase Cave = new LevelBase();
+    
+    //Game power ups
+    public PowerUpBase Health = new PowerUpBase();
+    public PowerUpBase Ammo = new PowerUpBase();
+
+    //Game weapons
+    private WeaponBase Gun = new WeaponBase();
+    private WeaponBase Rifle = new WeaponBase();
+
+    public string name;
+    private int score;
+}
         /*
         After prompt the game for a name we:
         Enter a cave
@@ -19,17 +49,3 @@ public class Game{
         If we win: get Health and ammo.
         If dragon wins: loose Health.
         */
-    }
-
-    //Game power ups
-    public PowerUpBase Health = new PowerUpBase();
-    public PowerUpBase Ammo = new PowerUpBase();
-    
-    //Game weapons
-    private WeaponBase Gun = new WeaponBase();
-    private WeaponBase Rifle = new WeaponBase();
-    private WeaponBase Knife = new WeaponBase();
-
-    public string name;
-    private int score;
-}
